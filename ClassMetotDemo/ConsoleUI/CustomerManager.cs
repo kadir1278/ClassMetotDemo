21455941662d2800ed98fbf6b10ac76383698adb
kadir1278 <kadirari@MAU-BIM5>
2021-02-22 14:16:41 +0300

    Proje Eklendi
    Proje Eklendi

---
+++ ClassMetotDemo/ConsoleUI/CustomerManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public class CustomerManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Ürün Eklendi");
+        }
+        public void Delete()
+        {
+            Console.WriteLine("Ürün Silindi");
+        }
+        public void Update()
+        {
+            Console.WriteLine("Ürün Güncellendi");
+        }
+        public void GetAll()
+        {
+            string[] Customer = {"Ürün 1","Ürün 2","Ürün 3" };
+            foreach (var item in Customer)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+            
+    }
+}
---
+++ ClassMetotDemo/ConsoleUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager _customer = new CustomerManager();
+            _customer.GetAll();
+            _customer.Add();
+            _customer.Delete();
+            _customer.Update();
+        }
+    }
+}
